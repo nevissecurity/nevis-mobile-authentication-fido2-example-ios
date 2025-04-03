d4791e679085677a64c6fbba560428cb878def44
Dániel Tóth <daniel.toth@nevis.net>
2025-04-03 13:34:10 +0200

    NEVISACCESSAPP-6558: fido2 poc
    - initial commit

---
+++ FIDO2/FIDO2App.swift
@@ -0,0 +1,17 @@
+//
+// FIDO2 PoC
+//
+// Copyright © 2025 NEVIS. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct FIDO2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
