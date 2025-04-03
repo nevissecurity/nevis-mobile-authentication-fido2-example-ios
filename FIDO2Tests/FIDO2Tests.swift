cfb64db549f34079a387a9751c859923829ec104
Dániel Tóth <daniel.toth@nevis.net>
2025-04-03 13:34:10 +0200

    NEVISACCESSAPP-6558: fido2 poc
    - initial commit

---
+++ FIDO2Tests/FIDO2Tests.swift
@@ -0,0 +1,17 @@
+//
+// FIDO2 PoC
+//
+// Copyright © 2025 NEVIS. All rights reserved.
+//
+
+
+import Testing
+@testable import FIDO2
+
+struct FIDO2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
