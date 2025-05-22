961593eb5833a0cddd5e3d27246cac3e3adf4063
Viktor Gulyás <viktor.gulyas@nevis.net>
2025-05-22 10:13:15 +0200

    NEVISACCESSAPP-6639: Removed using deprecated Swift UI methods
    - Additionally:
      - changed accent color to FIDO2 green
      - removed padding frame from the app icon

--- FIDO2/Presentation/Component/LoadingView.swift
+++ FIDO2/Presentation/Component/LoadingView.swift
@@ -30,9 +30,9 @@ struct LoadingView<Content>: View where Content: View {
 					width: geometry.size.width / 2,
 					height: geometry.size.height / 5
 				)
-				.background(Color.secondary.colorInvert())
-				.foregroundColor(.primary)
-				.cornerRadius(20)
+				.background { Color.secondary.colorInvert() }
+				.foregroundStyle(.primary)
+				.clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
 				.opacity(isShowing ? 1 : 0)
 			}
 		}
